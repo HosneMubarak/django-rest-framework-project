62803c5f7111a7da0152bab44a97e76626bd31a6
hsone mubarak <hsone.mubarak@accelx.net>
2021-11-17 18:01:14 +0600

    fix

---
+++ api_call.py
@@ -0,0 +1,34 @@
+import requests
+from requests.exceptions import HTTPError
+
+BASE_URL = "http://127.0.0.1:7000"
+
+
+def user_login_api(**args):
+    """
+    login api call
+    """
+    try:
+        login_url = BASE_URL + "/auth/token/login"
+
+        response = requests.post(
+            login_url,
+            headers={"Content-Type": "application/json"},
+            json={
+                "username": args['username'],
+                "password": args['password']
+            }
+        )
+
+        print(f"user_login_api(): response {response.json()}")
+
+        # If the response was successful, no Exception will be raised
+        response.status_code()
+    except HTTPError as http_err:
+        print(f'user_login_api(): HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'user_login_api(): Other error occurred: {err}')
+    else:
+        print('user_login_api(): Success!')
+
+    return response
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from api_call import user_login_api
 
 def login_view(requset):
+    if requset.POST:
+        username = requset.POST.get('username')
+        password = requset.POST.get('password')
+        print(username, password)
+
+        login_response = user_login_api(username=username, password=password)
+        print(login_response.json())
     return render(requset, 'frontend\login.html')
